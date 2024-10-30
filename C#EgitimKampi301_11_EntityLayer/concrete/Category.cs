4e7d40124e9a40cdae2c53341f1fed99d6d874aa
Akile  Nisa Öksüz <116032825+Akile-Nisa-Oksuz@users.noreply.github.com>
2024-10-30 18:42:55 +0300

    Add project files.

---
+++ C#EgitimKampi301_11_EntityLayer/concrete/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_EgitimKampi301_11_EntityLayer.concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set;}
+        public string CategoryName { get; set;}
+        public int CategoryStatus { get; set;}
+
+    }
+
+
+    /*// Field --> int x; direkt tanımlama
+         Varieble --> void test() metot içinde tanımlama
+                      {
+                        int z;
+                       }
+         Property ---> public int y {get; set;} */
+}
