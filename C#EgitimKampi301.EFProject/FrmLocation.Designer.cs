75183f82b860b19b1b4fb683a6f70327ebe1b9eb
Akile  Nisa Öksüz <116032825+Akile-Nisa-Oksuz@users.noreply.github.com>
2024-12-02 17:07:14 +0300

    Ders 16 - Entity Framework: Tur Projesi Location İşlemleri

---
+++ C#EgitimKampi301.EFProject/FrmLocation.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace C_EgitimKampi301.EFProject
+{
+    partial class FrmLocation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclose = new System.Windows.Forms.Button();
+            this.Upbtn = new System.Windows.Forms.Button();
+            this.Delbtn = new System.Windows.Forms.Button();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Listbtn = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSunMoon = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.downMax = new System.Windows.Forms.NumericUpDown();
+            this.combGuide = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.Color.Red;
+            this.btnclose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnclose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnclose.ForeColor = System.Drawing.Color.Gray;
+            this.btnclose.Location = new System.Drawing.Point(240, 610);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(80, 28);
+            this.btnclose.TabIndex = 25;
+            this.btnclose.Text = "Close";
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // Upbtn
+            // 
+            this.Upbtn.BackColor = System.Drawing.Color.Gainsboro;
+            this.Upbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Upbtn.ForeColor = System.Drawing.Color.Gray;
+            this.Upbtn.Location = new System.Drawing.Point(197, 500);
+            this.Upbtn.Name = "Upbtn";
+            this.Upbtn.Size = new System.Drawing.Size(123, 40);
+            this.Upbtn.TabIndex = 23;
+            this.Upbtn.Text = "Güncelle";
+            this.Upbtn.UseVisualStyleBackColor = false;
+            this.Upbtn.Click += new System.EventHandler(this.Upbtn_Click);
+            // 
+            // Delbtn
+            // 
+            this.Delbtn.BackColor = System.Drawing.Color.IndianRed;
+            this.Delbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Delbtn.ForeColor = System.Drawing.Color.Gray;
+            this.Delbtn.Location = new System.Drawing.Point(197, 454);
+            this.Delbtn.Name = "Delbtn";
+            this.Delbtn.Size = new System.Drawing.Size(123, 40);
+            this.Delbtn.TabIndex = 22;
+            this.Delbtn.Text = "Sil";
+            this.Delbtn.UseVisualStyleBackColor = false;
+            this.Delbtn.Click += new System.EventHandler(this.Delbtn_Click);
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Addbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Addbtn.ForeColor = System.Drawing.Color.Gray;
+            this.Addbtn.Location = new System.Drawing.Point(197, 408);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(123, 40);
+            this.Addbtn.TabIndex = 21;
+            this.Addbtn.Text = "Ekle";
+            this.Addbtn.UseVisualStyleBackColor = false;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(71, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 22);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Ülke";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(65, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 22);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Şehir";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(8, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 22);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Lokasyon ID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtCountry.Location = new System.Drawing.Point(134, 131);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(186, 22);
+            this.txtCountry.TabIndex = 20;
+            // 
+            // txtCity
+            // 
+            this.txtCity.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtCity.Location = new System.Drawing.Point(134, 86);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(186, 22);
+            this.txtCity.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(326, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(900, 632);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Listbtn
+            // 
+            this.Listbtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Listbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Listbtn.ForeColor = System.Drawing.Color.Gray;
+            this.Listbtn.Location = new System.Drawing.Point(197, 362);
+            this.Listbtn.Name = "Listbtn";
+            this.Listbtn.Size = new System.Drawing.Size(123, 40);
+            this.Listbtn.TabIndex = 15;
+            this.Listbtn.Text = "Listele";
+            this.Listbtn.UseVisualStyleBackColor = false;
+            this.Listbtn.Click += new System.EventHandler(this.Listbtn_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtId.Location = new System.Drawing.Point(134, 32);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(186, 22);
+            this.txtId.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(24, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 22);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Gün-Gece";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(68, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 22);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Fiyat";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label6.Location = new System.Drawing.Point(37, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 22);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Kapasite";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtSunMoon
+            // 
+            this.txtSunMoon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtSunMoon.Location = new System.Drawing.Point(134, 278);
+            this.txtSunMoon.Name = "txtSunMoon";
+            this.txtSunMoon.Size = new System.Drawing.Size(186, 22);
+            this.txtSunMoon.TabIndex = 31;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtPrice.Location = new System.Drawing.Point(134, 233);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(186, 22);
+            this.txtPrice.TabIndex = 29;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label7.Location = new System.Drawing.Point(48, 324);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 22);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Rehber";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // downMax
+            // 
+            this.downMax.Location = new System.Drawing.Point(134, 175);
+            this.downMax.Name = "downMax";
+            this.downMax.Size = new System.Drawing.Size(186, 22);
+            this.downMax.TabIndex = 34;
+            // 
+            // combGuide
+            // 
+            this.combGuide.FormattingEnabled = true;
+            this.combGuide.Location = new System.Drawing.Point(137, 322);
+            this.combGuide.Name = "combGuide";
+            this.combGuide.Size = new System.Drawing.Size(183, 24);
+            this.combGuide.TabIndex = 35;
+            // 
+            // FrmLocation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 656);
+            this.Controls.Add(this.combGuide);
+            this.Controls.Add(this.downMax);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSunMoon);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.Upbtn);
+            this.Controls.Add(this.Delbtn);
+            this.Controls.Add(this.Addbtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Listbtn);
+            this.Controls.Add(this.txtId);
+            this.Name = "FrmLocation";
+            this.Text = "FrmLocation";
+            this.Load += new System.EventHandler(this.FrmLocation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downMax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.Button Upbtn;
+        private System.Windows.Forms.Button Delbtn;
+        private System.Windows.Forms.Button Addbtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Listbtn;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSunMoon;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown downMax;
+        private System.Windows.Forms.ComboBox combGuide;
+    }
+}
---
+++ C#EgitimKampi301.EFProject/FrmLocation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_EgitimKampi301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+    
+
+        private void Listbtn_Click(object sender, EventArgs e)
+        {
+            var values = db.TblLocation.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values = db.TblGuide.Select(x=> new
+            {
+                FullName = x.GuideName + " " + x.GuideSurname,
+                x.GuideId
+            }).ToList();
+            combGuide.DisplayMember = "FullName";
+            combGuide.ValueMember = "GuideId";
+            combGuide.DataSource = values;
+        }
+
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            TblLocation location = new TblLocation();
+            location.LocationCapacity = byte.Parse(downMax.Value.ToString());
+            location.LocationCity=txtCity.Text;
+            location.LocationCountry=txtCountry.Text;
+            location.LocationPrice = decimal.Parse(txtPrice.Text);
+            location.DayNight=txtSunMoon.Text;
+            location.GuideId = int.Parse(combGuide.SelectedValue.ToString());
+            db.TblLocation.Add(location);
+            db.SaveChanges();
+            MessageBox.Show("Ekleme İşlemi Başarılı");
+        }
+
+        private void Delbtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var deletValue = db.TblLocation.Find(id);
+            db.TblLocation.Remove(deletValue);
+            db.SaveChanges();
+            MessageBox.Show("Silme işlemi yapıldı!");
+        }
+
+        private void Upbtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var upValue = db.TblLocation.Find(id);
+            upValue.LocationCapacity = byte.Parse(downMax.Value.ToString());
+            upValue.LocationCity = txtCity.Text;
+            upValue.LocationCountry = txtCountry.Text;
+            upValue.LocationPrice = decimal.Parse(txtPrice.Text);
+            upValue.DayNight = txtSunMoon.Text;
+            upValue.GuideId = int.Parse(combGuide.SelectedValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Güncelleme İşlemi başarılı");
+
+        }
+    }
+}
